dfd38a3823bbb32ede0c82b51c94314ff13d4e0b
Bryan Coutts <b2coutts@uwaterloo.ca>
2016-02-06 15:22:22 -0500

    Cleaned up graph generation script a bit

--- gen_graph.py
+++ gen_graph.py
@@ -2,18 +2,16 @@
 import networkx as nx
 from openopt import DSP
 
-avg_deg = 7.0
-n = 50
+def mkgraph(n, avg_deg):
+    G = nx.fast_gnp_random_graph(n, 1.0*avg_deg/n)
+    with open(('deg%s_n%s.graph' % (avg_deg, n)),  'w') as f:
+        f.write('%s\n' % n)
+        for u in G.nodes():
+            for v in G[u]:
+                f.write('%s %s\n' % (u,v))
+        f.write('-1 -1\n')
 
-G = nx.fast_gnp_random_graph(n, 1.0*avg_deg/n)
-#py_soln = nx.dominating_set(G)
-with open(('deg%s_n%s.graph' % (avg_deg, n)),  'w') as f:
-    f.write('%s\n' % n)
-    for u in G.nodes():
-        for v in G[u]:
-            f.write('%s %s\n' % (u,v))
-    f.write('-1 -1\n')
-
-p = DSP(G)
-r = p.solve('glpk', iprint=0)
-print(r.ff, r.solution)
+    p = DSP(G)
+    r = p.solve('glpk', iprint=0)
+    print(r.ff, r.solution)
+    return (G, p, r)
